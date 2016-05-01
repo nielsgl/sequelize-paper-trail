191fd2052fb6ff882f8a859727e715955b680225
Niels van Galen Last <nvangalenlast@gmail.com>
2016-04-30 21:14:31 -0700

    remove useless hello world files

--- src/helloworld.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-export default (): string => {
-  const message: string = 'Hello world!';
-  // console.log(message); // eslint-disable-line
-  return message;
-};
--- test/helloworld.spec.js
+++
@@ -1,7 +0,0 @@
-import app from 'helloworld';
-
-describe('app', () => {
-  it('returns string', () => {
-      expect(typeof(app())).to.equal('string');
-    });
-});
