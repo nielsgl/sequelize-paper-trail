00b79ff9a7c6776774743ee51a86d587f7ff4f31
Niels van Galen Last <nvangalenlast@gmail.com>
2019-03-15 14:09:24 +0100

    update main tests
    Signed-off-by: Niels van Galen Last <nvangalenlast@gmail.com>

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,19 +1,53 @@
 import SequelizeTrails from '../lib/index';
-var Sequelize = require('sequelize');
 
-describe('SequelizeTrails', () => {
-  it.skip('init returns object', () => {
-    var sequelize = new Sequelize('','','',{
-        dialect: 'sqlite',
-        logging: console.log
-    });
-    var User = sequelize.define('User', {
-        name: Sequelize.STRING
-    });
-    // console.log(sequelize);
-    // console.log(SequelizeTrails);
-    // console.log(SequelizeTrails(sequelize));
-    // console.log(typeof(SequelizeTrails));
-    expect(typeof(SequelizeTrails.init(sequelize))).to.equal('object');
-   });
+const db = require('../models/index.js');
+
+// const { Sequelize } = db;
+const { sequelize } = db;
+
+let User;
+let PaperTrails;
+
+describe('PaperTrails', () => {
+	it('initialize PaperTrails', done => {
+		PaperTrails = SequelizeTrails.init(sequelize, {
+			enableMigration: true,
+		});
+		PaperTrails.defineModels();
+		User = sequelize.model('User');
+		User.Revisions = User.hasPaperTrail();
+		User.refreshAttributes();
+		done();
+	});
+
+	it('model is revisionable', () => {
+		expect(User.revisionable).to.equal(true);
+	});
+
+	it('revision increments', done => {
+		User.findOrCreate({ where: { name: 'Dave' } })
+			.spread((user, created) => {
+				// console.log(user.get({plain: true}));
+				expect(created).to.equal(true);
+				expect(user.get('revision')).to.equal(1);
+				user.update({ name: 'David' })
+					.then(() => {
+						user.reload()
+							.then(() => {
+								// console.log(user.get({plain: true}));
+								expect(user.get('revision')).to.equal(2);
+								done();
+							})
+							.catch(err => {
+								done(err);
+							});
+					})
+					.catch(err => {
+						done(err);
+					});
+			})
+			.catch(err => {
+				done(err);
+			});
+	});
 });
