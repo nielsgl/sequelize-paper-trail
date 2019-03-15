9a4497ee401cf34b2f50e5d25db934376e83227e
Niels van Galen Last <nvangalenlast@gmail.com>
2019-03-15 14:00:34 +0100

    update helper tests
    Signed-off-by: Niels van Galen Last <nvangalenlast@gmail.com>

--- test/helpers.spec.js
+++ test/helpers.spec.js
@@ -1,138 +1,156 @@
 import helpers from '../lib/helpers';
 
 describe('calcDelta', () => {
-   it('returns number', () => {
-        // console.log('getDiffHelper', getDiffHelper())
-        var res = helpers.test();
-        expect(res).to.equal(true);
-    });
+	it('returns a difference in email addresses', () => {
+		const user1 = {
+			id: 1,
+			email: 'test@user.com',
+			encrypted_password:
+				'$2a$10$rNIOuy1pDl7bI5dTE.xMle/vcB51V2nJVpb/EBAwG4egXE42UfDMS',
+			reset_password_token: null,
+			reset_password_sent_at: null,
+			account_completed: false,
+		};
 
-    it('returns a difference in email addresses', () => {
-        var user1 = {
-            id: 1,
-            email: 'test@user.com',
-            encrypted_password: '$2a$10$rNIOuy1pDl7bI5dTE.xMle/vcB51V2nJVpb/EBAwG4egXE42UfDMS',
-            reset_password_token: null,
-            reset_password_sent_at: null,
-            account_completed: false
-        };
-        var user2 = {
-            id: 1,
-            email: 'test2@user.com',
-            encrypted_password: '$2a$10$rNIOuy1pDl7bI5dTE.xMle/vcB51V2nJVpb/EBAwG4egXE42UfDMS',
-            reset_password_token: null,
-            reset_password_sent_at: null,
-            account_completed: false
-        };
-        var exclude = ['id', 'created_at', 'updated_at'];
-        var res = helpers.calcDelta(user1, user2, exclude, true);
-        // console.log(res)
-        expect(true).to.equal(true);
-    });
+		const user2 = {
+			id: 1,
+			email: 'test2@user.com',
+			encrypted_password:
+				'$2a$10$rNIOuy1pDl7bI5dTE.xMle/vcB51V2nJVpb/EBAwG4egXE42UfDMS',
+			reset_password_token: null,
+			reset_password_sent_at: null,
+			account_completed: false,
+		};
 
-    it('returns a difference in null value', () => {
-        var user1 = {
-            id: 1,
-            email: 'test@user.com',
-            encrypted_password: '$2a$10$rNIOuy1pDl7bI5dTE.xMle/vcB51V2nJVpb/EBAwG4egXE42UfDMS',
-            reset_password_token: null,
-            reset_password_sent_at: null,
-            account_completed: false
-        };
-        var user2 = {
-            id: 1,
-            email: 'test@user.com',
-            encrypted_password: '$2a$10$rNIOuy1pDl7bI5dTE.xMle/vcB51V2nJVpb/EBAwG4egXE42UfDMS',
-            reset_password_token: true,
-            reset_password_sent_at: null,
-            account_completed: false
-        };
-        var exclude = ['id', 'created_at', 'updated_at'];
-        var res = helpers.calcDelta(user1, user2, exclude, true);
+		const exclude = ['id', 'created_at', 'updated_at'];
 
-        expect(true).to.equal(true);
-    });
+		const res = helpers.calcDelta(user1, user2, exclude, true);
 
-    it('returns a difference in a boolean', () => {
-        var user1 = {
-            id: 1,
-            email: 'test@user.com',
-            encrypted_password: '$2a$10$rNIOuy1pDl7bI5dTE.xMle/vcB51V2nJVpb/EBAwG4egXE42UfDMS',
-            reset_password_token: null,
-            reset_password_sent_at: null,
-            account_completed: false
-        };
-        var user2 = {
-            id: 1,
-            email: 'test@user.com',
-            encrypted_password: '$2a$10$rNIOuy1pDl7bI5dTE.xMle/vcB51V2nJVpb/EBAwG4egXE42UfDMS',
-            reset_password_token: null,
-            reset_password_sent_at: null,
-            account_completed: true
-        };
-        var exclude = ['id', 'created_at', 'updated_at'];
-        var res = helpers.calcDelta(user1, user2, exclude, true);
+		expect(res[0].lhs).to.equal('test@user.com');
+		expect(res[0].rhs).to.equal('test2@user.com');
+	});
 
-        expect(true).to.equal(true);
-    });
+	it('returns a difference in null value', () => {
+		const user1 = {
+			id: 1,
+			email: 'test@user.com',
+			encrypted_password:
+				'$2a$10$rNIOuy1pDl7bI5dTE.xMle/vcB51V2nJVpb/EBAwG4egXE42UfDMS',
+			reset_password_token: null,
+			reset_password_sent_at: null,
+			account_completed: false,
+		};
 
-    it('returns no difference in strings and numbers when strict is false', () => {
-        var obj1 = {
-            name: 'User',
-            age: '18'
-        };
+		const user2 = {
+			id: 1,
+			email: 'test@user.com',
+			encrypted_password:
+				'$2a$10$rNIOuy1pDl7bI5dTE.xMle/vcB51V2nJVpb/EBAwG4egXE42UfDMS',
+			reset_password_token: true,
+			reset_password_sent_at: null,
+			account_completed: false,
+		};
 
-        var obj2 = {
-            name: 'User',
-            age: 18
-        };
+		const exclude = ['id', 'created_at', 'updated_at'];
 
-        var res = helpers.calcDelta(obj1, obj2, [], false);
-        expect(true).to.equal(true);
-    });
+		const res = helpers.calcDelta(user1, user2, exclude, true);
 
-    it('returns a difference in strings and numbers when strict is true', () => {
-        var obj1 = {
-            name: 'User',
-            age: '18'
-        };
+		expect(res[0].lhs).to.equal(null);
+		expect(res[0].rhs).to.equal(true);
+	});
 
-        var obj2 = {
-            name: 'User',
-            age: 18
-        };
+	it('returns a difference in a boolean', () => {
+		const user1 = {
+			id: 1,
+			email: 'test@user.com',
+			encrypted_password:
+				'$2a$10$rNIOuy1pDl7bI5dTE.xMle/vcB51V2nJVpb/EBAwG4egXE42UfDMS',
+			reset_password_token: null,
+			reset_password_sent_at: null,
+			account_completed: false,
+		};
 
-        var res = helpers.calcDelta(obj1, obj2, [], true);
-        expect(true).to.equal(true);
-    });
+		const user2 = {
+			id: 1,
+			email: 'test@user.com',
+			encrypted_password:
+				'$2a$10$rNIOuy1pDl7bI5dTE.xMle/vcB51V2nJVpb/EBAwG4egXE42UfDMS',
+			reset_password_token: null,
+			reset_password_sent_at: null,
+			account_completed: true,
+		};
 
-    it('returns a difference in strings and numbers', () => {
-        var obj1 = {
-            name: 'User',
-            age: '18.1'
-        };
+		const exclude = ['id', 'created_at', 'updated_at'];
 
-        var obj2 = {
-            name: 'User',
-            age: 18.1
-        };
+		const res = helpers.calcDelta(user1, user2, exclude, true);
 
-        var res = helpers.calcDelta(obj1, obj2, [], false);
-        expect(true).to.equal(true);
-    });
+		expect(res[0].lhs).to.equal(false);
+		expect(res[0].rhs).to.equal(true);
+	});
 
-    it('returns a difference in strings and numbers', () => {
-        var obj1 = {
-            name: 'User',
-            age: '18.1'
-        };
+	it('returns no difference in strings and numbers when strict is false', () => {
+		const obj1 = {
+			name: 'User',
+			age: '18',
+		};
 
-        var obj2 = {
-            name: 'User1',
-            age: 18.1
-        };
+		const obj2 = {
+			name: 'User',
+			age: 18,
+		};
 
-        var res = helpers.calcDelta(obj1, obj2, [], false);
-        expect(true).to.equal(true);
-    });
+		const res = helpers.calcDelta(obj1, obj2, [], false);
+
+		expect(res).to.equal(null);
+	});
+
+	it('returns a difference in strings and numbers when strict is true', () => {
+		const obj1 = {
+			name: 'User',
+			age: '18',
+		};
+
+		const obj2 = {
+			name: 'User',
+			age: 18,
+		};
+
+		const res = helpers.calcDelta(obj1, obj2, [], true);
+
+		expect(typeof res[0].lhs).to.equal('string');
+		expect(typeof res[0].rhs).to.equal('number');
+	});
+
+	it('returns a difference in strings and decimals when strict is true', () => {
+		const obj1 = {
+			name: 'User',
+			age: '18.1',
+		};
+
+		const obj2 = {
+			name: 'User',
+			age: 18.1,
+		};
+
+		const res = helpers.calcDelta(obj1, obj2, [], true);
+
+		expect(typeof res[0].lhs).to.equal('string');
+		expect(typeof res[0].rhs).to.equal('number');
+	});
+
+	it('returns no difference in strings and decimals when strict is false', () => {
+		const obj1 = {
+			name: 'User',
+			age: '18.1',
+		};
+
+		const obj2 = {
+			name: 'User',
+			age: 18.1,
+		};
+
+		const res = helpers.calcDelta(obj1, obj2, [], false);
+
+		expect(res).to.equal(null);
+	});
 });
