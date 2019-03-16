ae55ac2ab91cd7ad0b0f6eab4c573564a42b0991
Niels van Galen Last <nvangalenlast@gmail.com>
2019-03-16 14:51:36 +0100

    async tests :heart:
    Signed-off-by: Niels van Galen Last <nvangalenlast@gmail.com>

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import SequelizeTrails from '../lib/index';
 
 const db = require('../models/index.js');
@@ -24,34 +25,93 @@ describe('PaperTrails', () => {
 		expect(User.revisionable).toEqual(true);
 	});
 
-	it('revision increments', done => {
-		expect.assertions(3);
+	it('can do something cool', async () => {
+		const [foo, bar] = await Promise.resolve(['foo', true]);
+		expect(bar).toEqual(true);
+	});
 
-		User.findOrCreate({ where: { name: 'Dave' } })
-			.spread((user, created) => {
-				// console.log(user.get({plain: true}));
+	describe('sets the revision for a model', () => {
+		it('creates the user', async () => {
+			expect.assertions(1);
 
-				expect(created).toEqual(true);
-				expect(user.get('revision')).toEqual(1);
-
-				user.update({ name: 'David' })
-					.then(() => {
-						user.reload()
-							.then(() => {
-								// console.log(user.get({plain: true}));
-								expect(user.get('revision')).toEqual(2);
-								done();
-							})
-							.catch(err => {
-								done(err);
-							});
-					})
-					.catch(err => {
-						done(err);
-					});
-			})
-			.catch(err => {
-				done(err);
+			const [user, created] = await User.findOrCreate({
+				where: { name: 'Dave' },
 			});
+			expect(created).toEqual(true);
+
+			// .spread((user, created) => {
+			// 	// console.log(user.get({plain: true}));
+
+			// 	expect(user.get('revision')).toEqual(1);
+
+			// 	user.update({ name: 'David' })
+			// 		.then(() => {
+			// 			user.reload()
+			// 				.then(() => {
+			// 					// console.log(user.get({plain: true}));
+			// 					expect(user.get('revision')).toEqual(2);
+			// 					done();
+			// 				})
+			// 				.catch(err => {
+			// 					done(err);
+			// 				});
+			// 		})
+			// 		.catch(err => {
+			// 			done(err);
+			// 		});
+			// })
+			// .catch(err => {
+			// 	done(err);
+			// });
+		});
+
+		it('is the first revision', async () => {
+			expect.assertions(1);
+
+			const res = await User.findOrCreate({
+				where: { name: 'Dave' },
+			});
+
+			expect(res[0].get('revision')).toEqual(1);
+
+			// .spread((user, created) => {
+			// 	// console.log(user.get({plain: true}));
+
+			// 	// expect(created).toEqual(true);
+
+			// 	user.update({ name: 'David' })
+			// 		.then(() => {
+			// 			user.reload()
+			// 				.then(() => {
+			// 					// console.log(user.get({plain: true}));
+			// 					expect(user.get('revision')).toEqual(2);
+			// 					done();
+			// 				})
+			// 				.catch(err => {
+			// 					done(err);
+			// 				});
+			// 		})
+			// 		.catch(err => {
+			// 			done(err);
+			// 		});
+			// })
+			// .catch(err => {
+			// 	done(err);
+			// });
+		});
+		it('increments the revision', async () => {
+			expect.assertions(1);
+
+			// eslint-disable-next-line prefer-const
+			let [user, created] = await User.findOrCreate({
+				where: { name: 'Dave' },
+			});
+
+			user = await user
+				.update({ name: 'David' })
+				.then(() => user.reload());
+
+			expect(user.get('revision')).toEqual(2);
+		});
 	});
 });
