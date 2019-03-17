50f6644d32d9444286245fdae851c1e76e945550
Niels van Galen Last <nvangalenlast@gmail.com>
2019-03-17 13:08:45 +0100

    move models and migrations for tests to tests dir
    Signed-off-by: Niels van Galen Last <nvangalenlast@gmail.com>

--- test/models/index.js
+++ test/models/index.js
@@ -5,7 +5,7 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'test';
 // eslint-disable-next-line import/no-dynamic-require
-const config = require(`${__dirname}/../config/config.json`)[env];
+const config = require(`${__dirname}/../../config/config.json`)[env];
 const db = {};
 
 let sequelize;
